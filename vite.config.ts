ef95ce05fdf24d5f84772ab0f7c96d5cebe037ba
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-12-20 15:24:37 +0530

    final changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	base: "/ravi-teja",
+	base: "/Kutumb",
 	plugins: [react()],
 	resolve: {
 		alias: {
